9fcae20e62991b4da753067c6fa314a717cdbe51
isaac <ims1991@hotmail.es>
2025-07-23 08:38:49 +0200

    lista urls

--- netlify/functions/tmdb.js
+++ netlify/functions/tmdb.js
@@ -2,18 +2,25 @@ export async function handler(event, context) {
   const API_KEY = process.env.TMDB_API_KEY;
   const page = event.queryStringParameters.page || 1;
 
-  const url_estrenos = `https://api.themoviedb.org/3/movie/upcoming?language=es-ES&region=ES&page=${page}&api_key=${API_KEY}`;
-  const url_cartelera = `https://api.themoviedb.org/3/movie/now_playing?language=es-ES&region=ES&page=${page}&api_key=${API_KEY}`;
-  url_list = [url_cartelera,url_estrenos]
+  const urls = [
+    `https://api.themoviedb.org/3/movie/upcoming?language=es-ES&region=ES&page=${page}&api_key=${API_KEY}`,
+    `https://api.themoviedb.org/3/movie/now_playing?language=es-ES&region=ES&page=${page}&api_key=${API_KEY}`
+  ];
+
   try {
-    for (url in url_list){
+    const allResults = [];
+
+    for (const url of urls) {
       const response = await fetch(url);
       const data = await response.json();
+      if (Array.isArray(data.results)) {
+        allResults.push(...data.results);
+      }
     }
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify({ results: allResults }), // ⬅️ Devuelve un array combinado
     };
   } catch (error) {
     return {
