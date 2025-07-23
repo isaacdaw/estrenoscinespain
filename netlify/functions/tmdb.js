b037bfc87a7192bda2fa97c61f0a6f31655ddce2
isaac <ims1991@hotmail.es>
2025-07-23 08:36:17 +0200

    intento dos urls

--- netlify/functions/tmdb.js
+++ netlify/functions/tmdb.js
@@ -2,11 +2,14 @@ export async function handler(event, context) {
   const API_KEY = process.env.TMDB_API_KEY;
   const page = event.queryStringParameters.page || 1;
 
-  const url = `https://api.themoviedb.org/3/movie/upcoming?language=es-ES&region=ES&page=${page}&api_key=${API_KEY}`;
-
+  const url_estrenos = `https://api.themoviedb.org/3/movie/upcoming?language=es-ES&region=ES&page=${page}&api_key=${API_KEY}`;
+  const url_cartelera = `https://api.themoviedb.org/3/movie/now_playing?language=es-ES&region=ES&page=${page}&api_key=${API_KEY}`;
+  url_list = [url_cartelera,url_estrenos]
   try {
-    const response = await fetch(url);
-    const data = await response.json();
+    for (url in url_list){
+      const response = await fetch(url);
+      const data = await response.json();
+    }
 
     return {
       statusCode: 200,
